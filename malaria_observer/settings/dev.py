b0ce48caa1044a72e00190cacb390f5b68b8b9ea
Lee <264464762+leehart3000@users.noreply.github.com>
2026-06-30 13:14:50 +0100

    Add Django, Wagtail using uv, Python 3.13, with .gitignore, without requirements.txt

---
+++ malaria_observer/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)9kxg106gknn%!4wpmmji-sl2a6+4309d=-*4(p2a)lw419fbl"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
